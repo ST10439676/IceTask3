cff806568ad7aa9a07e570606111410fa0c5b08f
ST10439676 <ST10439676@vcconnect.edu.za>
2025-03-13 14:43:05 +0200

    Implement Main Program
    Prompt users for rectangle or circle

--- IceTask3/Program.cs
+++ IceTask3/Program.cs
@@ -2,6 +2,44 @@
 
 class Program {
     static void Main(string[] args) {
-        Console.WriteLine("Hello, World!");
+        var menu = $"""
+                    (1) Rectangle 
+                    (2) Circle
+                    Choose Shape: 
+                    """;
+
+        Console.Write(menu);
+        var option = Console.ReadLine()!;
+        Shape? shape = null; 
+        switch (option) {
+            case "1":
+                Console.Write("Width: ");
+                var width = double.Parse(Console.ReadLine()!);
+                Console.Write("Height: ");
+                var height = double.Parse(Console.ReadLine()!);
+
+                shape = new Rectangle(width, height);                
+                
+                break;
+            case "2":
+                Console.Write("Radius: ");
+                var radius = int.Parse(Console.ReadLine()!);
+
+                shape = new Circle(radius);
+                break;
+            default:
+                // Nothing need to be done here shape is null;
+                break;
+            
+        }
+
+        
+        Console.WriteLine($"Displaying Shape {shape?.Name ?? "Unknown Shape"}");
+        // This won't display anything when shape is null
+        // Therefore if (shape is not null) is redundant.
+        // Error Checking is performed on the previous line,
+        // using the null coalescing operator (??).
+        shape?.Display();
+
     }
 }
